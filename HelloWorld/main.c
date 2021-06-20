330ee20d07f0ad4d7b2735e07a5722644dac0b98
Antonio Colucci <antonio.colucci003@studenti.uniparthenope.it>
2021-06-20 12:06:45 +0200

    Add files via upload

--- HelloWorld/main.c
+++ HelloWorld/main.c
@@ -5,10 +5,9 @@
 int main()
 {
 
-
     int id_thread, num_thread;
 
-    #pragma omp parallel private(id_thread) shared(num_thread) //creo un team di thread.
+    #pragma omp parallel private(id_thread), shared(num_thread) //creo un team di thread.
     {
       id_thread = omp_get_thread_num();
       num_thread = omp_get_num_threads();
@@ -17,8 +16,14 @@ int main()
 
     }
 
-//Per variare il numero dei core digitare nel terminale: export OMP_NUM_THREADS=2 (esempio con 2 threads).
-//Per verificare il tempo di esecuzione del programma utilizzare il comando time: time./hello.
+/*
+  Per variare il numero dei core digitare nel terminale: export OMP_NUM_THREADS=2 (esempio con 2 threads).
+  Per verificare il tempo di esecuzione del programma utilizzare il comando time: time./nome-eseguibile.
+  Questo comando restituisce tre informazioni:
+  - Real rappresenta il wall-clock time o elapsed-time, cioè il tempo effettivamente passato dall'inizio alla fine del programma.
+  - User, è il tempo speso in user-mode.
+  - Sys, è il tempo speso in servizi di sistema (Input/Output).
+*/
 
     return 0;
 }
